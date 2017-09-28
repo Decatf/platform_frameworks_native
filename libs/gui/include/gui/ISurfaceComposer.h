c6d39a19feb30aa744a6764255826380e4e25f7e
ryang <decatf@gmail.com>
2017-09-28 11:11:37 -0400

    libgui: Bring back support for mHeap-based screenshots
    Older graphics libraries throw a hissy fit when trying to lock
    buffers for Surface-based screenshots, on at least Tegra2/3 and
    Exynos4 hardware.
    
    This patch depends on the BOARD_USE_MHEAP_SCREENSHOT board flag
    and requires the ro.bq.gpu_to_cpu_unsupported property set to 1
    in order to work.
    
    [pawitp: port to Lollipop]
    
    Change-Id: I7db955e2cdd120018e349c14290e975788e70ed3

--- libs/gui/include/gui/ISurfaceComposer.h
+++ libs/gui/include/gui/ISurfaceComposer.h
@@ -26,6 +26,7 @@
 #include <utils/Vector.h>
 
 #include <binder/IInterface.h>
+#include <binder/IMemory.h>
 
 #include <ui/FrameStats.h>
 #include <ui/PixelFormat.h>
@@ -173,6 +174,18 @@ public:
             bool useIdentityTransform,
             Rotation rotation = eRotateNone) = 0;
 
+#ifdef USE_MHEAP_SCREENSHOT
+    /* Capture the specified screen. requires READ_FRAME_BUFFER permission
+     * This function will fail if there is a secure window on screen.
+     */
+    virtual status_t captureScreen(const sp<IBinder>& display, sp<IMemoryHeap>* heap,
+            uint32_t* width, uint32_t* height,
+            Rect sourceCrop, uint32_t reqWidth, uint32_t reqHeight,
+            uint32_t minLayerZ, uint32_t maxLayerZ,
+            bool useIdentityTransform,
+            Rotation rotation = eRotateNone) = 0;
+#endif
+
     /* Clears the frame statistics for animations.
      *
      * Requires the ACCESS_SURFACE_FLINGER permission.
@@ -214,6 +227,9 @@ public:
         SET_TRANSACTION_STATE,
         AUTHENTICATE_SURFACE,
         GET_SUPPORTED_FRAME_TIMESTAMPS,
+#ifdef USE_MHEAP_SCREENSHOT
+        CAPTURE_SCREEN_DEPRECATED,
+#endif
         GET_DISPLAY_CONFIGS,
         GET_ACTIVE_CONFIG,
         SET_ACTIVE_CONFIG,
